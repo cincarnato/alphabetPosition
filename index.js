025c65eca128a5851a21074195bb35b28a7ae0a7
Cristian Incarnato <cristian.cdi@gmail.com>
2021-02-25 23:20:28 -0300

    Solved

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const alphabetPosition = function(str){
-    
+    let abc = "abcdefghijklmnopqrstuvwxyz".split("")
     /* CODE */
-    
+    let arr = str.toLowerCase().split("")
+    let numArr = []
+
+    for(let i = 0; i < arr.length;i++){
+        let index = abc.indexOf(arr[i])
+        if(index != -1){
+            numArr.push(index+1)
+        }
+    }
+
+    return numArr.join(" ")
 }
 
 module.exports = alphabetPosition
